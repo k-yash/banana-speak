e565ee3d027461d3691314bd393bc8a359c65a29
yash1703 <50240587+yash1703@users.noreply.github.com>
2020-11-30 00:22:31 +0530

    feat : new features updated

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input")
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#output");
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+getTranslationURL = (text) => serverURL + "?" +"text="+ text;
+
+errorHandler = (error) => {
+    console.log("Error Occured ",error);
+    alert("something wrong with server! try again in some time");
+}
 
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!!");
-    console.log(txtInput.value);
+    // txtOutput.innerText = "output is" + txtInput.value
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>{
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText;
+    } )
+    .catch(errorHandler);
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var textArea = document.querySelector("textarea")
+// var btnprimary = document.querySelector(".btn-primary")
+// var btninput = document.querySelector("#input-btn")
+// var name = document.querySelector("input[name='translator']")
